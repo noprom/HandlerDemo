79d78ba0b49f3bde7f3555a144c8c2abef9e1970
noprom <874702417@qq.com>
2014-12-30 05:59:48 +0800

    use msg's obj properity
    use msg's obj properity

--- src/main/java/com/noprom/handler/MainActivity.java
+++ src/main/java/com/noprom/handler/MainActivity.java
@@ -1,25 +1,42 @@
 package com.noprom.handler;
 
+import android.app.Activity;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     private TextView textView;
-    private Handler handler = new Handler(){
+    private Button button;
+    private Handler handler = new Handler() {
         // handler处理消息
         @Override
         public void handleMessage(Message msg) {
-            textView.setText(""+msg.arg1);
+//            textView.setText("" + msg.arg1 + "-" + msg.arg2);、
+            textView.setText(msg.obj + "");
         }
     };
+
+    @Override
+    public void onClick(View v) {
+        handler.removeCallbacks(myRunnable);
+    }
+
+    class Person {
+        public int age;
+        public String name;
+
+        @Override
+        public String toString() {
+            return "name = " + name + "age = " + age;
+        }
+    }
 
 
     private ImageView imageView;
@@ -29,6 +46,7 @@ public class MainActivity extends ActionBarActivity {
     private int index;
     // 开启子线程
     private MyRunnable myRunnable = new MyRunnable();
+
     class MyRunnable implements Runnable {
 
         @Override
@@ -36,7 +54,7 @@ public class MainActivity extends ActionBarActivity {
             index++;
             index = index % 4;
             imageView.setImageResource(images[index]);
-            handler.postDelayed(myRunnable,1000);
+            handler.postDelayed(myRunnable, 1000);
         }
     }
 
@@ -45,30 +63,40 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 初始化View
-//        textView = (TextView) findViewById(R.id.id_textview);
+        textView = (TextView) findViewById(R.id.id_textview);
         imageView = (ImageView) findViewById(R.id.id_imageView);
+        button = (Button) findViewById(R.id.id_button);
+        button.setOnClickListener(this);
 
-        new Thread(new Runnable() {
+
+        new Thread() {
             @Override
             public void run() {
                 try {
                     Thread.sleep(2000);
-                    Message msg = new Message();
-                    msg.arg1 = 100;
-                    // 发送方法
-                    handler.sendMessage(msg);
+//                    Message msg = new Message();
+//                    msg.arg1 = 100;
+//                    msg.arg2 = 200;
 
+
+                    Message msg = handler.obtainMessage();
+                    // 指定msg的obj
+                    Person person = new Person();
+                    person.age = 25;
+                    person.name = "小明";
+                    msg.obj = person;
+
+                    // 发送方法
+//                    handler.sendMessage(msg);
+                    msg.sendToTarget();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-        }).start();
+        }.start();
 
 
-
-
-
-//        handler.postDelayed(myRunnable,1000);
+        handler.postDelayed(myRunnable,1000);
 
 
 //
